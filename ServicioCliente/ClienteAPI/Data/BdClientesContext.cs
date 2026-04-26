44ce133df8847293043e13d0a46a4ac02a31f935
Usher Halanocca Rojas <uh2023076795@virtual.upt.pe>
2026-04-26 14:29:38 -0500

    Añadido proyecto ServicioCliente con controladores y Dockerfile

---
+++ ServicioCliente/ClienteAPI/Data/BdClientesContext.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using ClienteAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClienteAPI.Data;
+
+public partial class BdClientesContext : DbContext
+{
+    public BdClientesContext()
+    {
+    }
+
+    public BdClientesContext(DbContextOptions<BdClientesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<ClientesDocumento> ClientesDocumentos { get; set; }
+
+    public virtual DbSet<TiposDocumento> TiposDocumentos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,16111;Database=BD_CLIENTES;User Id=sa;Password=Upt.2022;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.IdCliente);
+
+            entity.ToTable("CLIENTES");
+
+            entity.Property(e => e.IdCliente).HasColumnName("ID_CLIENTE");
+            entity.Property(e => e.NomCliente)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("NOM_CLIENTE");
+        });
+
+        modelBuilder.Entity<ClientesDocumento>(entity =>
+        {
+            entity.HasKey(e => new { e.IdCliente, e.IdTipoDocumento });
+
+            entity.ToTable("CLIENTES_DOCUMENTOS");
+
+            entity.Property(e => e.IdCliente).HasColumnName("ID_CLIENTE");
+            entity.Property(e => e.IdTipoDocumento).HasColumnName("ID_TIPO_DOCUMENTO");
+            entity.Property(e => e.NumDocumento)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("NUM_DOCUMENTO");
+
+            entity.HasOne(d => d.IdClienteNavigation).WithMany(p => p.ClientesDocumentos)
+                .HasForeignKey(d => d.IdCliente)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_CLIENTES_DOCUMENTOS_X_CLIENTE");
+
+            entity.HasOne(d => d.IdTipoDocumentoNavigation).WithMany(p => p.ClientesDocumentos)
+                .HasForeignKey(d => d.IdTipoDocumento)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_CLIENTES_DOCUMENTOS_X_TIPO_DOCUMENTO");
+        });
+
+        modelBuilder.Entity<TiposDocumento>(entity =>
+        {
+            entity.HasKey(e => e.IdTipoDocumento);
+
+            entity.ToTable("TIPOS_DOCUMENTOS");
+
+            entity.Property(e => e.IdTipoDocumento)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("ID_TIPO_DOCUMENTO");
+            entity.Property(e => e.DesTipoDocumento)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("DES_TIPO_DOCUMENTO");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ServicioCliente/ClienteAPI/Program.cs
@@ -0,0 +1,46 @@
+using ClienteAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<BdClientesContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ClienteDB")));
+
+// ✅ Swagger con Swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// ✅ Middleware de Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
